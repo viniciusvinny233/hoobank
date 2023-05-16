022d63b07cb1940b16ce9bb2ad1cdf3e0616f730
Marcos Vinicius <viniciusvinny233@gmail.com>
2023-05-16 16:39:43 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <h1 className="bg-primary w-full- text-white 
+    text-3xl font-bold">
+      Hello world!
+    </h1>
+  )
+}
+
+
